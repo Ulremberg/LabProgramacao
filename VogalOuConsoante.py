0dad4efb2d0c32f87abaf37f33abaf92c2bf0881
ulremberg <berguebarboza@hotmail.com>
2019-09-03 11:15:35 -0300

    add file

---
+++ VogalOuConsoante.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+a = raw_input("Digite uma letra:\n").lower()
+
+if(a == 'a' or a == 'e' or a == 'i' or a == 'o' or a == 'u'):
+    print('A letra é uma vogal')
+else:
+    print('A letra é uma consoante')
