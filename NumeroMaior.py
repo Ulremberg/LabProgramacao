321c1863439328fc5aac1682dc47f1452614893b
ulremberg <berguebarboza@hotmail.com>
2019-09-03 11:16:25 -0300

    add file

---
+++ NumeroMaior.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+numero1 = int(input("Digite primero valor:\n"))
+numero2 = int(input("Digite segundo valor:\n"))
+
+if(numero1 > numero2):
+    print('O maior valor digitado foi: {}'.format(numero1))
+elif(numero2 > numero1):
+    print('O maior valor digitado foi: {}'.format(numero2))
