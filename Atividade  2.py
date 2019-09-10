7370695c4f2a7d931bc4e6027d312a21267fca5e
ulremberg <berguebarboza@hotmail.com>
2019-09-09 22:35:55 -0300

    add file

---
+++ Atividade  2.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+c = 1
+while c <= 50:
+  print(c)
+  c += 2
---
+++ Atividade 1.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# num = 1
+#
+# while( num <= 20):
+#     print(num)
+#     num +=1
+
+# num = 1
+# while (num <= 20):
+#     print("{}".format(num))
+#     num += 1
+
+#num1 = str(num)
+i = 1
+for i in range(1):
+    print(list(range(21)))
