eac0d05fda9965e0d751c4b66cff6e5a82680dfa
ulremberg <berguebarboza@hotmail.com>
2019-09-26 12:33:45 -0300

    add file

---
+++ exerciselista4.py
@@ -0,0 +1,12 @@
+#lista 5
+
+inteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+v1 = inteiros[0]
+v2 = inteiros[1]
+v3 = inteiros[2]
+v4 = inteiros[3]
+v5 = inteiros[4]
+v6 = inteiros[5]
+v7 = inteiros[6]
+v8 = inteiros[7]
