c01114b12ddde4c4a68f4f3da1b7935d9afbce54
ulremberg <berguebarboza@hotmail.com>
2019-09-27 09:32:17 -0300

    fix

--- exerciselista4.py
+++ exerciselista4.py
@@ -1,12 +1,14 @@
 #lista 5
 
-inteiros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+even = []
+odd = []
 
-v1 = inteiros[0]
-v2 = inteiros[1]
-v3 = inteiros[2]
-v4 = inteiros[3]
-v5 = inteiros[4]
-v6 = inteiros[5]
-v7 = inteiros[6]
-v8 = inteiros[7]
+for i in range(0, 20):
+    number = int(input('Enter value:\n'))
+    if(number % 2 == 0):
+        even.append(number)
+    else:
+        odd.append(number)
+
+print('The values even is: {}'.format(even))
+print('The value odd is: {}'.format(odd))
