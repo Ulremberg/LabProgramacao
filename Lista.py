64910a9f77a5055e17fa2431e3a683c03fb84c82
ulremberg <berguebarboza@hotmail.com>
2019-10-11 21:07:32 -0300

    add file

---
+++ Lista.py
@@ -0,0 +1,46 @@
+#
+# num = []
+#
+# for i in range (0, 10):
+#     num.append(int(input('Digite um numero\n')))
+#
+# print(num)
+
+# num = [1,2,3,4,5,6]
+#
+# print(num)
+#
+valores = [1,2,3,4,5,6,7]
+#
+# print(len(num))
+# print(len(valores))
+#
+# num.append(12)
+#
+# print(num)
+#
+# valores.insert(7,8)
+#
+# print(valores)
+#
+# valores.pop(7)
+#
+# print(valores)
+
+mix = [12,2, "Jessica",4,1,6,3,97]
+
+ver = [9,8,7,6,5,4,3,2,1]
+
+print(list(reversed(ver)))
+print(sorted(valores))
+sume = [1,2]
+print(sum(sume))
+
+
+print(valores)
+print(min(ver))
+print(max(ver))
+
+print(ver.count(1))
+
+print(valores)
