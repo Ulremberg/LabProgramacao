d7962a31e81ff0bb39e919a7d79524451f52b8fe
ulremberg <berguebarboza@hotmail.com>
2019-10-18 18:12:10 -0300

    add file

---
+++ CalculoSalário.py
@@ -0,0 +1,7 @@
+remuneracao_hora = int(input())
+horas = int(input())
+total = remuneracao_hora * horas
+print("Informe quanto ganha por hora:")
+print("Informe a quantidade mensal de trabalho:")
+print("Resultado: %.1f" % total)
+print('Resultado: {:.1f}'.format(total))
