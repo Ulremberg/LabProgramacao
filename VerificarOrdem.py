c4d76cac4bce35eae1229a29f948c8ae3c6e6d76
ulremberg <berguebarboza@hotmail.com>
2019-10-23 11:13:32 -0300

    add file

---
+++ VerificarOrdem.py
@@ -0,0 +1,8 @@
+entrada1, entrada2 = map(int, input().split())
+
+if (entrada1 < entrada2):
+    print(entrada1, entrada2)
+elif (entrada1 > entrada2):
+    print(entrada2, entrada1)
+else:
+    print(entrada1, entrada2)
