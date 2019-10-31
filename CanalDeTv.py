dc106a85aebd0fdb2cbbc9b835278baad830dc68
ulremberg <berguebarboza@hotmail.com>
2019-10-31 14:19:55 -0300

    add file

---
+++ CanalDeTv.py
@@ -0,0 +1,20 @@
+canal = int(input())
+#4 band 2 sbt 6 REDETV 9 RECORD
+if (canal == 13):
+    print("Digite um numero de um canal de tv:")
+    print("GLOBO")
+elif (canal == 9):
+    print("Digite um numero de um canal de tv:")
+    print("RECORD")
+elif (canal == 6):
+    print("Digite um numero de um canal de tv:")
+    print("REDETV!")
+elif (canal == 4):
+    print("Digite um numero de um canal de tv:")
+    print("BAND")
+elif (canal == 2):
+    print("Digite um numero de um canal de tv:")
+    print("SBT")
+else:
+    print("Digite um numero de um canal de tv:")
+    print("Canal Invalido")
